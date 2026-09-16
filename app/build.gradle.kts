d6e5411c2ede3be4c7dc44f4380b3e63109ac6b2
safer27-arch <safer27@gmail.com>
2026-09-17 00:36:36 +0200

    Fix v1.1.2 Guide resource linking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.example.electrodecutcompare"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.1.1"
+        versionCode = 16
+        versionName = "1.1.2"
     }
 }
